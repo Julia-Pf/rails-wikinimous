3de11f9dbb039efbcee9d7c45f0008a67d222a25
Julia Pfaffenzeller <julia.pfaffenzeller@web.de>
2024-01-23 16:20:39 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   Rails.application.routes.draw do
-    resources :restaurants
+    resources :articles
   end
 end
